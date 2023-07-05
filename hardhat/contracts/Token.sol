7220a652368b80fe2e15d5794a98f9584b1f5d3d
Valerio Preti <valerio.preti@gmail.com>
2023-07-05 23:31:56 +0200

    add hardhat console debug in solidity smart contract

--- hardhat/contracts/Token.sol
+++ hardhat/contracts/Token.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
+
 contract Token {
     string public name = "HardhHat Token";
     string public symbol = "HHT";
@@ -16,6 +18,8 @@ contract Token {
     }
 
     function transfer(address to, uint amount) external {
+        console.log("Sender balance is %s token", balances[msg.sender]);
+        console.log("Sender is sending %s tokens to %s address", amount, to);
         require(balances[msg.sender] >= amount, "Not enough tokens");
         balances[msg.sender] -= amount;
         balances[to] += amount;
