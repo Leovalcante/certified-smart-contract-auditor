9a50366534e407d6a28ef17e4ddca9c77fc4655f
Valerio Preti <valerio.preti@gmail.com>
2023-07-07 18:15:32 +0200

    init foundry stake contract

---
+++ example/src/StakeContract.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+error StakeContract__TransferFailer();
+
+contract StakeContract {
+    mapping(address => uint) public s_balances;
+
+    function stake(
+        uint amount,
+        address token
+    ) external view returns (bool success) {
+        s_balances[msg.sender] += amount;
+        bool success = IERC20(token).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+
+        if (!success) revert StakeContract__TransferFailer();
+
+        return success;
+    }
+}
